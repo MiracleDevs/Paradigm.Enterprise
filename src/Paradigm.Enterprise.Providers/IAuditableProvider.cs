733b826043c87444eb76bada77a44200c65f499e
Iván Falletti <ivan@miracledevs.com>
2025-05-05 11:44:22 -0300

    Removed not needed IAuditableProvider interface

--- src/Paradigm.Enterprise.Providers/IAuditableProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using Paradigm.Enterprise.Domain.Entities;
-
-namespace Paradigm.Enterprise.Providers;
-
-public interface IAuditableProvider<TView> : IEditProvider<TView>
-    where TView : EntityBase, new()
-{
-}
